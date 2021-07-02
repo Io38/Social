fed4608d2a10da4c9627eb2d893c7da7f2edfafa
Io38 <purumpurumpum620@gmail.com>
2021-07-02 14:06:35 +0300

    margin:auto;

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -8,9 +8,10 @@ const Profile = () => {
         <article className={q.profile}>
 
             <div>
-
-                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    alt="Error 404 " className={q.avatar} ></img>
+                <div className={q.avatarka}>
+                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                        alt="Error 404 " className={q.avatar} ></img>
+                </div>
                 Name Sirname<br />
                 description<br />
                 Posts:<br /><br /><br /><br /><br />
