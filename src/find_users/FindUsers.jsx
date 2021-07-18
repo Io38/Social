bb0579c5acbfcfed27839d9ebeb94c6c5db73c5a
Io38 <purumpurumpum620@gmail.com>
2021-07-18 08:22:58 +0300

    follow/unfollow

--- src/find_users/FindUsers.jsx
+++ src/find_users/FindUsers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import * as axios from 'axios';
 import React from 'react';
 import q from "./FindUsers.module.css"
@@ -12,7 +13,8 @@ class FindUsers extends React.Component {
 
     componentDidMount() {
         this.props.setIsLoading(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+            { withCredentials: true })
             .then(response => {
                 this.props.setIsLoading(false);
                 this.props.setUsers(response.data.items);
@@ -26,7 +28,8 @@ class FindUsers extends React.Component {
     onPageChange = (e) => {
 
         this.props.setPage(e);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+            { withCredentials: true })
             .then(response => {
 
                 this.props.setUsers(response.data.items);
@@ -68,13 +71,52 @@ class FindUsers extends React.Component {
                                 <span>
                                     <div>
                                         <NavLink to={'/profile' + u.id}>
-                                            <img src={u.photos.small == null ? photo : u.photos.small} className={q.ava} alt="failed to download the pic" />
+                                            <img src={u.photos.small == null ? photo : u.photos.small} className={q.ava} />
                                         </NavLink>
                                     </div>
 
                                     <div>{u.friend ?
-                                        <button onClick={() => { this.props.unFriend(u.id) }} >Remove from a friend list</button> :
-                                        <button onClick={() => { this.props.friend(u.id) }}>Add to friend list</button>}</div>
+                                        <button onClick={() => {
+                                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+
+                                                withCredentials: true,
+                                                headers: {
+                                                    "API-KEY": "4bfabc4e-fb16-4cce-b138-421ee880ae4d"
+                                                }
+
+                                            })
+                                                .then(response => {
+
+                                                    if (response.data.resultCode === 0) {
+
+                                                        this.props.unFriend(u.id);
+                                                    }
+                                                })
+
+
+                                        }} >
+                                            Remove from a friend list</button> :
+
+                                        <button onClick={() => {
+                                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+
+                                                withCredentials: true,
+                                                headers: {
+                                                    "API-KEY": "4bfabc4e-fb16-4cce-b138-421ee880ae4d"
+                                                }
+
+                                            })
+                                                .then(response => {
+
+                                                    if (response.data.resultCode === 0) {
+
+                                                        this.props.friend(u.id);
+                                                    }
+                                                })
+
+
+                                        }}>Add to friend list</button>}
+                                    </div>
 
                                     <span>
 
