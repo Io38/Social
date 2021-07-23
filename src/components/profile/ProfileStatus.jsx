3a5fb1a8af14dc8aa52215aa631a83833bf0e135
Io38 <purumpurumpum620@gmail.com>
2021-07-23 11:32:20 +0300

    componentDidUpdate  in ProfileStatus

--- src/components/profile/ProfileStatus.jsx
+++ src/components/profile/ProfileStatus.jsx
@@ -29,6 +29,17 @@ class ProfileStatus extends React.Component {
         })
     }
 
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevState.status !== this.props.status) {
+
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
 
         return <div>
